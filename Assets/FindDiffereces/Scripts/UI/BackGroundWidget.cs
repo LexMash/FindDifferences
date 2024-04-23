c57ab4356e4c19f83ffa24b1b602c1c5a8ca9f8d
LexMash <101925088+LexMash@users.noreply.github.com>
2024-04-23 09:23:29 +0300

    some visual added

---
+++ Assets/FindDiffereces/Scripts/UI/BackGroundWidget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace FindDiffereces.UI
+{
+    public class BackGroundWidget : MonoBehaviour
+    {
+        [SerializeField] private RawImage _rawImage;
+        [SerializeField] private float _scrollSpeed;
+        [SerializeField] private Vector2 _scrollDirection;
+
+        private void Update()
+        {
+            _rawImage.uvRect = new Rect(_rawImage.uvRect.position + _scrollDirection.normalized * _scrollSpeed * Time.deltaTime, _rawImage.uvRect.size);
+        }
+    }
+}
